4e19b2a27d74a06d27f2aa866ed7f2df85575c60
woodjecket <1164806786@qq.com>
2023-01-24 14:18:42 +0800

    d8 Try

---
+++ src/crazy/content/ScalaItem.scala
@@ -0,0 +1,10 @@
+package crazy.content
+
+import arc.graphics.Color
+import mindustry.`type`.Item
+
+object ScalaItem{
+  val electronicIron: Item = new Item("iron",Color.valueOf("123456")){
+    charge = 0.1f
+  }
+}
