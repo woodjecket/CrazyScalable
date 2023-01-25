31c74385fda64cf79ddb884bd9ae78ef1a874940
woodjecket <1164806786@qq.com>
2023-01-25 14:28:03 +0800

    2.12.5 test

--- src/crazy/CrazyScalaMod.scala
+++ src/crazy/CrazyScalaMod.scala
@@ -3,17 +3,14 @@ package crazy
 import arc.util.Log
 import mindustry.Vars
 import mindustry.mod._
-
-import scala.collection.IterableOnce.iterableOnceExtensionMethods
-import scala.collection.mutable
+import mindustry.`type`._
 
 class CrazyScalaMod() extends Mod(){
   Log.info("Loaded ExampleScalaMod constructor.","")
   override def loadContent(): Unit = {
     Log.info("Loading some example content.","")
-    Log.info("Sort as id","")
-    Vars.content.items().toArray.toList.map(i => (i, i.id)).sortWith((l1,_)=>l1._1.compareTo(l1._1)<0).foreach (i => Log.info("@ is id @", i._2, i._1))
-    Log.info("Sort as name", "")
-    Vars.content.items().toArray.toList.map(i => (i, i.id)).sortWith ((_, l1) => l1._1.compareTo(l1._1) < 0).foreach (i => Log.info("@ is id @", i._2, i._1))
+    var itemList:List[(Item,Short)] = Vars.content.items().toArray.toList.map(i => (i, i.id))
+    val sortedList = itemList.sortWith((l1,_)=>l1._2.compareTo(l1._2)>0)
+    sortedList.foreach[Unit](i => Log.info("@ is id @", i._2.asInstanceOf[Item],i._1))
   }
 }
