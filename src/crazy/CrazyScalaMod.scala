774347a4743b0a0cc9da13de936f1aa710e832e7
woodjecket <1164806786@qq.com>
2023-01-25 19:20:02 +0800

    fix

--- src/crazy/CrazyScalaMod.scala
+++ src/crazy/CrazyScalaMod.scala
@@ -9,8 +9,9 @@ class CrazyScalaMod() extends Mod(){
   Log.info("Loaded ExampleScalaMod constructor.","")
   override def loadContent(): Unit = {
     Log.info("Loading some example content.","")
-    var itemList:List[(Item,Short)] = Vars.content.items().toArray.toList.map(i => (i, i.id))
+    val itemList:List[(Item,Short)] = Vars.content.items().toArray.toList.map(i => (i, i.id))
     val sortedList = itemList.sortWith((l1,_)=>l1._2.compareTo(l1._2)>0)
-    sortedList.foreach[Unit](i => Log.info("@ is id @", i._2.asInstanceOf[Item],i._1))
+    sortedList.foreach[Unit](i => Log.info("@ is id @", i._1.asInstanceOf[AnyRef], i._2.asInstanceOf[AnyRef]))
+    Log.info("RUN SCALA ON ANDROID!!!!!!!!!!!!!!!!","")
   }
 }
