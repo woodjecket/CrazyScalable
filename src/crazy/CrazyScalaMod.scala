b04ff85ff984885024e929a621dfb4d892cd27f3
woodjecket <1164806786@qq.com>
2023-01-25 19:20:28 +0800

    fix

--- src/crazy/CrazyScalaMod.scala
+++ src/crazy/CrazyScalaMod.scala
@@ -12,6 +12,6 @@ class CrazyScalaMod() extends Mod(){
     val itemList:List[(Item,Short)] = Vars.content.items().toArray.toList.map(i => (i, i.id))
     val sortedList = itemList.sortWith((l1,_)=>l1._2.compareTo(l1._2)>0)
     sortedList.foreach[Unit](i => Log.info("@ is id @", i._1.asInstanceOf[AnyRef], i._2.asInstanceOf[AnyRef]))
-    Log.info("RUN SCALA ON ANDROID!!!!!!!!!!!!!!!!","")
+    if(Vars.android)Log.info("RUN SCALA ON ANDROID!!!!!!!!!!!!!!!!","")
   }
 }
